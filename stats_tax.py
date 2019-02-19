cc370e4e94797bf68074b9b339b4c7b9ef33a6e9
tetedange13 <felix.deslacs@gmail.com>
2019-02-19 16:24:42 +0100

    Added a separate file for computation of stats (FP, TP etc)

---
+++ stats_tax.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+
+"""
+"""
+
+
+import sys, os
+import pandas as pd
+
+
+def handle_strain(sp_name, rank):
+    """
+    Deals with 'strain' issues (i.e. organism that have been detected beyond
+    the species level taxonomicly = 'no rank')
+    Cut the name of the organism to keep only the 'Genus species' form and
+    attributes the taxonomic level 'strain' to it  
+    """
+    splitted_sp_name = sp_name.split()
+    
+    # Very likely a strain if name longer than 2:
+    if rank in ('no rank', 'subspecies') and len(splitted_sp_name) > 2: 
+        # We keep only the first 2 words and we change the rank value:
+        new_sp_name, new_rank_sp = " ".join(splitted_sp_name[0:2]), "strain"
+        
+        return (new_sp_name, new_rank_sp)
+    
+    else:
+        return (sp_name, rank)
+
+
+def in_zymo(sp_name, sp_rank, taxo_level_cutoff):
+    """
+    Given the rank of a BLAST hit and a sublist of taxonomic levels, deducted
+    from the cutoff (for the taxonomic level),  returns a string in 
+    ('TP', 'FP', 'TN', 'FN').
+    The name of the species is also requiered, to deal with 'strain' cases
+    """
+    taxo_levels = ['superkingdom', 'phylum', 'class', 'order', 'family', 
+                   'genus', 'species', 'subspecies'] + ["strain"]
+    # Define the species contained within the Zymo mock community:
+    list_prok = ['Listeria monocytogenes', 'Bacillus subtilis', 
+                 'Staphylococcus aureus', 'Escherichia coli', 
+                 'Lactobacillus fermentum', 'Enterococcus faecalis',
+                 'Pseudomonas aeruginosa', 'Salmonella enterica']
+    list_euk = ['Cryptococcus neoformans', 'Saccharomyces cerevisiae']
+    sublist_taxo = taxo_levels[taxo_levels.index(taxo_level_cutoff): ]
+    
+    # We handle the issues associated with 'strains'
+    new_name, rew_rank = handle_strain(sp_name, sp_rank)
+    
+    if rew_rank in sublist_taxo: # Assigned
+        if new_name in list_prok:
+            return 'TP' # True Positive
+        elif new_name in list_euk:
+            return 'TN' # True Negative
+        else:
+            return 'FP' # False Positive
+    
+    else:
+        return 'FN'
+
