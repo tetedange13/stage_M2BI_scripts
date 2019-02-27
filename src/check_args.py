da591c92582914e76653ff6183268b6a67f6c4f3
tetedange13 <felix.deslacs@gmail.com>
2019-02-27 10:04:10 +0100

    Made func checking infile more general

--- src/check_args.py
+++ src/check_args.py
@@ -9,27 +9,28 @@ Contains all functions linked with checking of arguments given as input
 import os, sys
 
 
-def check_input_fq(input_fq_path):
+def check_infile(infile_path, list_acceptable_ext):
     """
-    Check if everything's fine with the fq file given as input
+    Check if everything's fine with the fq or fa file given as input
     """
-    if not os.path.isfile(input_fq_path):
+    if not os.path.isfile(infile_path):
         print("ERROR! Wrong specified input fastq file")
         sys.exit(2)
     else:
-        head_input_fq, tail_input_fq = os.path.split(input_fq_path)
-        input_fq_base, input_fq_ext = os.path.splitext(tail_input_fq)
+        head_infile, tail_infile = os.path.split(infile_path)
+        infile_base, infile_ext = os.path.splitext(tail_infile)
         
-        if input_fq_ext[1:] not in ("fq", "fastq"):
-            err_ext = ("ERROR! Current only fastq file (ext=fq or " +
-                       "ext=fastq) are accepted")
+        if infile_ext[1:] not in list_acceptable_ext:
+            err_ext = ("ERROR! The given file as the wrong extension. " +
+                       "Should be in:" + list_acceptable_ext)
             print(err_ext)
             sys.exit(2)
         
         else:
-            return (input_fq_path, input_fq_base)
-
-
+            return (infile_path, infile_base, head_infile, tail_infile, 
+                    infile_ext)
+            
+            
 def check_input_nb(input_nb):
     """
     Check if a integer given as argument is valid
@@ -56,3 +57,33 @@ def check_input_taxo_cutoff(input_cutoff, list_taxo):
         print("Must be in:", list_taxo, '\n')
         sys.exit(2)
 
+
+def check_bool_type(rep):
+    """
+    Check if the answer is boolean type.
+    Args:
+        rep: answer given by the user (str)
+    Returns:
+        The answer casted into a boolean
+    """
+    rep = rep.lower()
+    if rep in ("true", "t", "yes", "y"):
+        return True
+    elif rep in ("false", "f", "no", "n"):
+        return False
+    else:
+        print("Enter a boolean type (True, T, False, F) !")
+        sys.exit(2)
+        
+        
+def check_soft(trim, list_softs):
+    """
+    Check if a suitable name of soft has been given as argument
+    """
+    trim = trim.lower()
+    if trim in ["no"] + list_softs:
+        return trim
+    else:
+        print("Enter a valid soft name or 'no' (default) !")
+        sys.exit(2)
+        
