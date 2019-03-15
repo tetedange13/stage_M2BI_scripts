9c93819f7f18c2a5c3e6c05b214e0bcc0115cadc
tetedange13 <felix.deslacs@gmail.com>
2019-03-15 14:37:49 +0100

    Erased taxid mapping with local parallel search

--- build_custom_CentriIndex.py
+++ build_custom_CentriIndex.py
@@ -51,8 +51,8 @@ def map_headers_to_taxid(dirOutput, in_fa_file, taxa_id):
 if __name__ == "__main__":
     # Get arguments:
     ARGS = docopt(__doc__, version='0.1')
-    # input_db_path = check.infile(ARGS["--infileDB"], 
-    #                              ('fa', 'fasta', 'fq', 'fastq'))[0]
+    input_db_path = check.infile(ARGS["--infileDB"], 
+                                 ('fa', 'fasta', 'fq', 'fastq'))[0]
     dirOut = ARGS["--dirOut"]                            
     list_gi_path = ARGS["--accList"]
     NB_THREADS = int(check.input_nb(ARGS["--threads"]))
@@ -104,7 +104,6 @@ if __name__ == "__main__":
                     seqid2taxid_file.write(header + '\t' + 
                                            dict_seqid2taxid[start_header] + 
                                            '\n')
-
                 else:
                     splitted_header = header.split()[0]
                     sp_name = conv_headers_Zymo[splitted_header]
@@ -114,145 +113,9 @@ if __name__ == "__main__":
 
             seqid2taxid_file.close()
 
-
         elif id_type == "gi":
             print("MARCHE PLUS !\n")
             sys.exit()
-            # TAXID_START_TIME = t.time()
-
-            # # Get set
-            # with open(list_gi_path, 'r') as list_gi_file:
-            #    set_acc = {line.rstrip('\n').split()[1] for line in list_gi_file}
-            # assert(set_acc) # Be sure it is NOT empty
-
-            # nb_to_find, nb_found = len(set_acc), 0
-            # to_acc2taxid = osp.join(dirOut, "acc2taxid")
-            # to_need_remote = osp.join(dirOut, "need_remote")
-            
-            # if not osp.isfile(to_need_remote) and not osp.isfile(to_acc2taxid):
-            #     # Parallel local taxid mapping:
-            #     to_gz_acc2taxid = (to_dbs + "nt_db/taxo_18feb19/" +
-            #                        "nucl_wgs.accession2taxid.gz")
-            #     size_chunks = 1000000
-            #     results = []
-            #     pool = mp.Pool(NB_THREADS)
-            #     with gzip.open(to_gz_acc2taxid, 'rt') as acc2taxid_gz:
-            #         acc2taxid_gz.readline() # Skip header
-            #         worker = partial(pll.taxid_mapping, set_accession=set_acc)
-            #         print("\nMapping taxids against accession numbers...")
-
-            #         compt_cut_done = 1
-            #         while True:
-            #             print("cut", compt_cut_done, ":", 
-            #                   NB_THREADS*size_chunks)
-            #             compt_cut_done += 1
-            #             groups = [list(ittls.islice(acc2taxid_gz, size_chunks)) for i in range(NB_THREADS)]
-
-            #             # As soon as 1st element not an empty list
-            #             # (if the 1st one IS empty, the following will beb too)
-            #             if groups[0]: 
-            #                 result = pool.imap_unordered(worker, groups)
-            #                 results.extend(result)
-            #             else:
-            #                 print("TOUT CONSOMME")
-            #                 break
-
-            #     pool.close()
-            #     print("PARALLEL SEARCH TIME:", t.time() - TAXID_START_TIME)
-
-            #     dict_acc2taxid = {} # To convert accession number to taxid
-            #     set_to_remote = set() # Not found locally, will need remote search
-            #     acc2taxid_file = open(to_acc2taxid, 'w')
-
-            #     for res in results:
-            #         if res: # If  NOT 'None'
-            #             for found_acc_nb, found_taxid in res:
-            #                 dict_acc2taxid[found_acc_nb] = found_taxid
-            #                 acc2taxid_file.write(found_acc_nb + '\t' + 
-            #                                      found_taxid + '\n')
-            #     acc2taxid_file.close()
-
-            #     found_acc_numbers = dict_acc2taxid.keys()
-            #     nb_found = len(found_acc_numbers)
-            #     print("TO_FIND:", nb_to_find)
-            #     print("--> FOUND:", nb_found)
-            #     print(nb_to_find - nb_found, "acc_nb couldn't be found locally!")
-            #     print()
-
-            #     if nb_found == 0:
-            #         os.remove(to_acc2taxid)
-
-            #     with open(to_need_remote, 'w') as need_remote_file:
-            #         for acc_nb in set_acc:
-            #             if acc_nb not in found_acc_numbers:
-            #                 need_remote_file.write(acc_nb + '\n')
-            #     sys.exit()
-
-            # # We solve the problems by querying remotely the taxids
-            # if osp.isfile(to_need_remote):
-            #     if osp.isfile(to_acc2taxid):
-            #         dict_acc2taxid = {}
-            #         with open(to_acc2taxid, 'r') as acc2taxid_file:
-            #             for line in acc2taxid_file:
-            #                 acc_nb, taxid = line.rstrip('\n').split('\t')
-            #                 dict_acc2taxid[acc_nb] = taxid
-            #     else:
-            #         dict_acc2taxid = {}
-
-            #     # Some need to be added manually:
-            #     dict_acc2taxid["CP006690"] = "1051650"
-            #     dict_acc2taxid["CDRP01002148"] = "562"
-            #     dict_acc2taxid["FOMU01000028"] = "321267"
-            #     dict_acc2taxid["CP001098"] = "373903"
-            #     dict_acc2taxid["LIGM01000002"] = "337330"
-            #     dict_acc2taxid["MCIE01000002"] = "46170"
-            #     dict_acc2taxid["JTBM01000001"] = "28901"
-
-            #     set_to_remote = set()
-            #     with open(to_need_remote, 'r') as need_remote_file:
-            #         for line in need_remote_file:
-            #             currrent_acc_nb = line.rstrip('\n')
-            #             if currrent_acc_nb not in dict_acc2taxid.keys():
-            #                 set_to_remote.add(currrent_acc_nb)
-
-            #     print("REMAINING ONE QUERIED REMOTELY...")
-            #     for pb_acc_nb in set_to_remote:
-            #         # print("QUERYING REMOTELY:", problematic_acc_nb)
-            #         queried_taxid = remote.taxid_from_gb_entry(pb_acc_nb)
-            #         dict_acc2taxid[pb_acc_nb] = queried_taxid
-            #         with open(to_acc2taxid, 'a') as acc2taxid_file:
-            #             acc2taxid_file.write(pb_acc_nb + '\t' + 
-            #                                  queried_taxid + '\n')
-                
-            #     print("REMOTE SEARCH FINISHED !\n")
-            #     sys.exit()
-
-            
-            # # Write seqid2taxid file:
-            # to_seqid2taxid = osp.join(dirOut, "seqid2taxid")
-            # found_acc_numbers = dict_acc2taxid.keys()
-
-            # with open(list_gi_path, 'r') as list_gi_file, \
-            #      open(to_seqid2taxid, 'w') as seqid2taxid_file:
-            #     for line in list_gi_file:
-            #         seqid, acc_nb = line.rstrip('\n').split('\t')
-
-            #         if acc_nb in found_acc_numbers:
-            #             seqid2taxid_file.write(seqid + '\t' + 
-            #                                    dict_acc2taxid[acc_nb] + '\n')
-            #         else:
-            #             print("STILL A PB..")
-            #             os.remove(to_seqid2taxid)
-            #             sys.exit(2)
-            
-
-            # # os.remove(to_need_remote)
-            # sys.exit()
-                
-            # print("TOTAL TAXID SEARCH RUNTIME:", t.time() - TAXID_START_TIME)
-            
-            # # Get list of taxids to give to 'centrifuge-download':
-            # # set_taxids = dict_acc2taxid.values()
 
 
     else:
