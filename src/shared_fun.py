fbde96b4e77a7b77767be1b95155142e61d9f691
tetedange13 <felix.deslacs@gmail.com>
2019-03-19 17:28:35 +0100

    Commented the old in_zymo() func for further suppression

--- src/shared_fun.py
+++ src/shared_fun.py
@@ -51,25 +51,25 @@ def generate_sets_zymo(taxonomic_cutoff):
     return (set_prok, set_euk)
         
 
-def in_zymo(current_taxid, taxonomic_cutoff, tupl_sets):
-    """
-    Given the rank of a BLAST hit and a sublist of taxonomic levels, deducted
-    from the cutoff (for the taxonomic level),  returns a string in 
-    ('TP', 'FP', 'TN', 'FN').
-    The name of the species is also requiered, to deal with 'strain' cases
-    """
-    # print(taxfoo.get_taxid_name(current_taxid), end='\t')
-    current_lineage = taxfoo.get_lineage_as_dict(current_taxid)
-    if taxonomic_cutoff not in current_lineage.keys():
-        return 'FP' # Taxo cutoff not in keys, so False Positive ?
+# def in_zymo(current_taxid, taxonomic_cutoff, tupl_sets):
+#     """
+#     Given the rank of a BLAST hit and a sublist of taxonomic levels, deducted
+#     from the cutoff (for the taxonomic level),  returns a string in 
+#     ('TP', 'FP', 'TN', 'FN').
+#     The name of the species is also requiered, to deal with 'strain' cases
+#     """
+#     # print(taxfoo.get_taxid_name(current_taxid), end='\t')
+#     current_lineage = taxfoo.get_lineage_as_dict(current_taxid)
+#     if taxonomic_cutoff not in current_lineage.keys():
+#         return 'FP' # Taxo cutoff not in keys, so False Positive ?
 
-    current_taxo_level = current_lineage[taxonomic_cutoff]
-    assert(current_taxo_level)
+#     current_taxo_level = current_lineage[taxonomic_cutoff]
+#     assert(current_taxo_level)
 
-    set_levels_prok, set_levels_euk = tupl_sets
-    if current_taxo_level in set_levels_prok: # TP
-        return 'TP'
-    elif current_taxo_level in set_levels_euk: # TN
-        return 'TN'
-    else: # False Positive
-        return 'FP'
+#     set_levels_prok, set_levels_euk = tupl_sets
+#     if current_taxo_level in set_levels_prok: # TP
+#         return 'TP'
+#     elif current_taxo_level in set_levels_euk: # TN
+#         return 'TN'
+#     else: # False Positive
+#         return 'FP'
