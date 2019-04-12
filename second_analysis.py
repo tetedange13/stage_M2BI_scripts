1fdab106c2ec03ffd389d03a6fe4acb45081e556
tetedange13 <felix.deslacs@gmail.com>
2019-04-12 17:05:48 +0200

    Finished to code L1dist & log-modulus calculation

--- second_analysis.py
+++ second_analysis.py
@@ -16,7 +16,7 @@ Remarks:
 1) The OTUs table given as input must be properly formatted (as it is
 explained on qiime.org) and be at the SPECIES level
 2) Ideally the file resulting from this cmd: 
-`summarize_taxa.py -i my_biom_w_metadat.biom -o . --level 7 -a --suppress_biom_table_output`
+`summarize_taxa.py -i my_biom_w_metadat.biom -o . --level 7 --absolute_abundance --suppress_biom_table_output`
 """
 
 import os, sys
@@ -59,30 +59,7 @@ def plot_pie_chart(pdSeries_to_plot, print_percents, arg_title=""):
     axis.set_ylabel('') # To remove auto legends sided to the chart
     fig.subplots_adjust(left=-0.4)
     axis.set_title(arg_title)
-
-
-def calc_L1dist_logModulus(pdSeries_obs_abund, pdSeries_expect_abund):
-    """
-    Compute both the L1-distance and the vector of log-modulus values between 
-    expected and observed abundances
-
-    From: https://genomebiology.biomedcentral.com/track/pdf/10.1186/s13059-017-1299-7
-    "While the log-modulus examines a fold-change, the L1 distance shows the 
-    distance between relative abundance vectors by dataset"
-    """
-    log_modulus = lambda difference: (pd.np.sign(difference) * 
-                                      pd.np.log10(1 + pd.np.abs(difference)))
-
-    dict_L1dist,dict_log_modulus = {}, {}
-    for sp_name in pdSeries_obs_abund.index:
-        diff = pdSeries_obs_abund[sp_name] - pdSeries_expect_abund[sp_name]
-        dict_L1dist[sp_name] = pd.np.abs(diff)
-        dict_log_modulus[sp_name] = log_modulus(diff)
-
-    # print(dict_L1dist)
-    return (sum(dict_L1dist.values()), 
-            pd.Series(dict_log_modulus).sort_values(ascending = False))
-
+    
 
 
 # MAIN
@@ -104,27 +81,12 @@ if __name__ == '__main__':
     counts_sp_obs = gether_counts(my_taxo_csv, taxo_cutoff, 'tot_sp_obs')
 
     # Draw pie chart of abundances (simple counts):
-    plot_pie_chart(counts_sp_obs, False, "MON BEAU CAMEMBERT")
+    # plot_pie_chart(counts_sp_obs, False, "MON BEAU CAMEMBERT")
     # plt.show()
 
     df_prok_ref = eval.generate_df_zymo()
-    df_prok_ref = df_prok_ref.assign(abs_count=df_prok_ref['rel_count']*sum(counts_sp_obs)/100)
-
-
-    # Gether all FP into a 'misassigned' category:
-    # /!\ 'no_majo_found' are ignored /!\
-    # dict_species2count = {'misassigned':0}
-    # for counted_item, count in counts_species.items():
-    #     if counted_item != 'no_majo_found':
-    #         if dict_species2res[counted_item] == 'FP':
-    #             dict_species2count['misassigned'] += count
-    #         else:
-    #             dict_species2count[counted_item] = count
-    # del counted_item
-
-    # counts_only_zymo = pd.Series(dict_species2count).sort_values(ascending=False)
-    # print(counts_only_zymo)
-
+    df_prok_ref = df_prok_ref.assign(abs_count=df_prok_ref.rel_count * 
+                                               sum(counts_sp_obs) / 100)
 
     df_counts = pd.DataFrame(gether_counts(df_prok_ref, taxo_cutoff, 
                              'expect_counts')) 
@@ -132,7 +94,7 @@ if __name__ == '__main__':
     set_expected = df_counts.index
     df_counts.loc['misassigned', 'expect_counts'] = 0 # Add misassigned
     df_counts.expect_counts = pd.to_numeric(df_counts.expect_counts,
-                                              downcast='integer')
+                                            downcast='integer')
 
     # Gether all 'misassigned' together:
     dict_tmp = {'misassigned':0}
@@ -141,34 +103,23 @@ if __name__ == '__main__':
             dict_tmp['misassigned'] += count
         else:
             dict_tmp[taxa_name] = count
-    print(dict_tmp)
+    del taxa_name, count
 
-    df_counts = df_counts.assign(obs_counts=dict_tmp.values())
+    df_counts = df_counts.assign(obs_counts=[dict_tmp[tax_name] 
+                                             for tax_name in df_counts.index])
+    df_counts = df_counts.assign(difference=df_counts.obs_counts - 
+                                            df_counts.expect_counts)
+
+    log_modulus = lambda diff: (pd.np.sign(diff) * pd.np.log10(1 + pd.np.abs(diff)))
+    df_counts = df_counts.assign(log_modulus=lambda x: log_modulus(x.difference),
+                                 L1dist=lambda x: pd.np.abs(x.difference))
+    
     print(df_counts)
-    sys.exit()
+    print("L1-distance =", sum(df_counts.L1dist))
+
+    # sys.exit()
 
     print()
-    plot_pie_chart(counts_only_zymo, True, "MON BEAU CAMEMBERT")
+    plot_pie_chart(df_counts.obs_counts.sort_values(ascending=False), True, 
+                   "Absolute abundances\n at the {} level".format(taxo_cutoff))
     plt.show()
-
-    if taxo_cutoff == 'species':
-        
-        # print(counts_only_zymo.drop(['misassigned']))
-
-        not_misassigned = counts_only_zymo.drop(['misassigned'])
-        tot_reads_zymo = sum(not_misassigned)
-        foo_dict = {sp_name:expected_percents[sp_name]*tot_reads_zymo/100 
-                    for sp_name in expected_percents.keys()}
-        # print(pd.Series(test))
-        a_tupl = calc_L1dist_logModulus(not_misassigned, 
-                                        pd.Series(foo_dict))
-        L1dist, vect_logModulus = a_tupl
-        # print("L1-distance =", L1dist)
-        print()
-        # print(vect_logModulus)
-        # print(counts_species[map(lambda val: val.split()[0] == 'Bacillus', counts_species.index)])
-        # plt.show()
-        # sys.exit()
-
-
-
