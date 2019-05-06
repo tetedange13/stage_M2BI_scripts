bd1999c64c0a5b6e3f562b707f5ee6d26831cfbc
tetedange13 <felix.deslacs@gmail.com>
2019-05-06 16:44:13 +0200

    Made code more flexible

--- make_stacked_barplots.R
+++ make_stacked_barplots.R
@@ -11,33 +11,51 @@ if (length(args) == 0) {
 library(dplyr)
 library(ggplot2)
 library(reshape2)
+library(stringr)
 
 # test  <- data.frame(person=c("A", "B", "C", "D", "E"), 
 #                     value1=c(100,150,120,80,150),     
 #                     value2=c(25,30,45,30,30) , 
 #                     value3=c(100,120,150,150,200))
-# print(test)
 #melted <- melt(test, "person")
 #print(melted$variable == 'value1')
-
 # melted$cat <- ''
 # melted[melted$variable == 'value1',]$cat <- "first"
 # melted[melted$variable != 'value1',]$cat <- "second"
 
 df2 <- read.csv(data_to_load, header=T, sep=';')
 melted <- melt(df2, "run")
+# print(melted)
 
-cond_raw <- (melted$variable == 'FDR' | melted$variable == 'sens')
-cond_lenFilt <- (melted$variable == 'FDR_lenFilt' | 
-                 melted$variable == 'sens_lenFilt')
-cond_topOne <- (melted$variable == 'FDR_topOne' | 
-                 melted$variable == 'sens_topOne')
+test <- str_locate(melted$variable, '_')[, "start"]
+vect_debut <- c()
+vect_end <- c() 
+for (i in 1:length(melted$variable)) {
+    cond_name <- toString(melted[i, "variable"])
+    vect_debut <- c(vect_debut, 
+                 substr(cond_name, start=1, 
+                        stop=test[i]-1))
+    vect_end <- c(vect_end, 
+                 substr(cond_name, start=test[i]+1, 
+                        stop=nchar(cond_name)))
+}
+# warnings()
+melted$variable <- factor(vect_debut)
+melted$cat <- factor(vect_end)
+# print(melted)
+# exit()
 
-melted$cat <- ''
-melted[cond_raw, ]$cat <- "1_Raw"
-melted[cond_lenFilt, ]$cat <- "2_LenFilt"
-# melted[cond_topOne, ]$cat <- "3_TopOne"
-melted[cond_lenFilt, ]$variable <- melted[cond_raw, ]$variable
+# cond_raw <- (melted$variable == 'FDR' | melted$variable == 'sens')
+# cond_lenFilt <- (melted$variable == 'FDR_lenFilt' | 
+#                  melted$variable == 'sens_lenFilt')
+# cond_topOne <- (melted$variable == 'FDR_topOne' | 
+#                  melted$variable == 'sens_topOne')
+
+# melted$cat <- ''
+# melted[cond_raw, ]$cat <- "1_Centri_Raw"
+# melted[cond_lenFilt, ]$cat <- "2_Centri_Filt"
+# # melted[cond_topOne, ]$cat <- "3_TopOne"
+# melted[cond_lenFilt, ]$variable <- melted[cond_raw, ]$variable
 
 # melted <- ddply(melted, c("run", "cat"),
 #                    transform, label_ypos=value-cumsum(value))
@@ -61,10 +79,11 @@ ggplot(melted, aes(x = cat, y = value, fill = variable)) +
   geom_text(aes(y=label_ypos, label=value), vjust=-0.5, 
             color="white", size=3.5) +
   facet_grid(~ run) +
-  ggtitle(paste("Mapping (p08N25) -", title_plot, "- lvl SPECIES")) + 
+  ggtitle(paste(title_plot, "- lvl SPECIES")) + 
   theme(plot.title = element_text(hjust = 0.5), 
         panel.background = element_rect(fill = "grey"),
-        panel.grid.major = element_blank()) +
+        panel.grid.major = element_blank(),
+        axis.text.x = element_text(face="bold")) +
   scale_y_continuous(name="recall + FDR", 
                      breaks=c(0, 0.25, 0.5, 0.75, 1))
 
