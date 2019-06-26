a9747caa93b1c53ec20d65ac1e36e349733f8697
tetedange13 <felix.deslacs@gmail.com>
2019-06-26 18:49:42 +0200

    Started change from 'pre-built Minimap2 index' (had problems) to 'on-the-fly' Minimap2 index

--- test_pipeline.py
+++ test_pipeline.py
@@ -161,7 +161,22 @@ if __name__ == "__main__":
         # dirOut_minimap = path_proj + "3-deter_minimap2/"
         dirOut_minimap = path_proj + "3-deter_minimap2_second/"
 
-        args_minimap2_map = "-N25 -t" + nb_threads + " -ax map-ont "
+        # Determine path to fasta of reference DB:
+        if DB_NAME == 'zymo':
+            to_ref_fa = path_proj + 'zymo_SEGO.fa' 
+        elif DB_NAME == 'rrn':
+            to_ref_fa = to_dbs + 'rrn_8feb19/operons.100.fa'
+        elif DB_NAME == 'silva':
+            to_ref_fa = (to_dbs + 'SILVA_refNR132_28feb19/' + 
+                         'SILVA_132_SSURef_Nr99_tax_silva.fasta')
+        elif DB_NAME == 'ncbi_16s':
+            to_ref_fa = to_dbs + 'NCBI_16S_18-06-2019/bacteria.16SrRNA.fna'
+        else: # Has to be Silva db
+            print("DB not correct with Minimap2..")
+            sys.exit()
+
+        args_minimap2_map = "-K300M -N25 -t" + nb_threads + " -ax map-ont "
+        # args_minimap2_map = "-N25 -t" + nb_threads + " -ax map-ont "
         print("  >> WITH PARAM:", args_minimap2_map)
 
         to_minimap_idxes = to_dbs + "minimap2_idxes/"
